9730cabd32e6a96e0d795fae2b940c11473f4814
buildsmile <you@example.com>
2022-06-25 11:53:56 +0700

    init tabpages

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gsb_workshop/pages/tabs/setting.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,7 +8,13 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+final tabs = [
+  Center(child: Text('Home')),
+  SettingScreen(),
+];
+
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,21 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: Icon(Icons.inventory),
         title: Text('Inventory'),
       ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+
+  _onItemTapped(int index) {
+    _currentIndex = index;
+    setState(() {});
   }
 }
