ce44e1f7174817cb23187b41e3019994c173e2a5
buildsmile <you@example.com>
2022-06-26 11:12:30 +0700

    update complete

--- lib/pages/inventories/updateInventory.dart
+++ lib/pages/inventories/updateInventory.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gsb_workshop/components/StockForm.dart';
+import 'package:gsb_workshop/models/HttpStatusMsg.dart';
 import 'package:gsb_workshop/models/Inventory.dart';
+import 'package:gsb_workshop/services/InventoryService.dart';
+import 'package:gsb_workshop/utils/AlertHelper.dart';
+import 'package:gsb_workshop/utils/LoadingProgress.dart';
 
 class UpdateInventory extends StatefulWidget {
   const UpdateInventory({Key? key}) : super(key: key);
@@ -30,5 +34,17 @@ class _UpdateInventoryState extends State<UpdateInventory> {
     );
   }
 
-  _submit(Inventory inv) {}
+  _submit(Inventory inv) {
+    LoadingProgress.inProgress(() async {
+      InventoryService inventoryService = InventoryService();
+      HttpStatusMsg htm = await inventoryService.update(inv);
+      if (htm.success) {
+        AlertHelper.showBar(context: context, msg: 'Update Complete');
+        Navigator.pop(context);
+      } else {
+        AlertHelper.showBar(
+            context: context, msg: htm.errorMsg!, isError: true);
+      }
+    });
+  }
 }
